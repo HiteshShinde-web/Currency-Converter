d8629b3f4efb77a2e89d813322e17c82fc11e8d1
Hitesh Shinde <hiteshshinde8777@gmail.com>
2024-08-10 20:16:54 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import "./App.css";
 import CurrencyConvertor from "./components/currency-convertor";
 
+
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+   
+
+    
+    <div className="head">
       <div className="container">
         <CurrencyConvertor />
       </div>
     </div>
+
+    
   );
 }
 
